9d97634d5e3beef5f2fc5f5b87dbeaf1d6cc3e8f
Ismael Damasceno Tristão <ismaeldamascenotris@gmail.com>
2023-06-04 12:33:05 -0300

    Belt offset adicionado

--- unity/Assets/Objects/Kitchen/Belt/BeltOffset.cs
+++ unity/Assets/Objects/Kitchen/Belt/BeltOffset.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR.Haptics;
 
 public class BeltOffset : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public static float _Velocidade = 0.1f;
+    public static BeltOffset _instancia;
+    private static Material _materialBelt;
+    private static float _texturaAltura;
+    private static Vector2 _offset;
+
+
+    private void Awake()
+    {
+        if (_instancia == null)
+        {
+            _instancia = this;
+            Iniciar();
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _offset.y -= (Time.deltaTime * _Velocidade) % _texturaAltura;
+        _materialBelt.SetTextureOffset("_MainTex", _offset);
     }
+
+    public static void Iniciar()
+    {
+        _materialBelt = _instancia.GetComponent<Renderer>().material;
+        _texturaAltura = _materialBelt.GetTexture("_MainTex").height;
+    }
+
 }
